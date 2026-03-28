e677496007dfcb7b1f7e5f8ea4b10620efe2c418
lee-kyu-hwan <lgh778923@gmail.com>
2026-03-28 21:23:31 +0900

    feat(nvim): 코어 설정 (lazy.nvim, options, keymaps)

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+local map = vim.keymap.set
+
+-- 창 이동
+map("n", "<C-h>", "<C-w>h", { desc = "왼쪽 창으로" })
+map("n", "<C-j>", "<C-w>j", { desc = "아래 창으로" })
+map("n", "<C-k>", "<C-w>k", { desc = "위 창으로" })
+map("n", "<C-l>", "<C-w>l", { desc = "오른쪽 창으로" })
+
+-- 버퍼
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "이전 버퍼" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "다음 버퍼" })
+
+-- ESC로 검색 하이라이트 제거
+map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "검색 하이라이트 제거" })
+
+-- 비주얼 모드에서 들여쓰기 유지
+map("v", "<", "<gv")
+map("v", ">", ">gv")
---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,23 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.clipboard = "unnamedplus"
+opt.ignorecase = true
+opt.smartcase = true
+opt.termguicolors = true
+opt.splitbelow = true
+opt.splitright = true
+opt.undofile = true
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
