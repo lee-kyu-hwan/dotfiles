71c9336c62340b143b25db28645e79449121992f
이규환 <lgh778923@gmail.com>
2026-03-31 12:54:16 +0900

    Add .config/nvim/lsp/eslint.lua

---
+++ dot_config/nvim/lsp/eslint.lua
@@ -0,0 +1,13 @@
+return {
+  root_markers = {
+    "eslint.config.js",
+    "eslint.config.mjs",
+    "eslint.config.cjs",
+    ".eslintrc.js",
+    ".eslintrc.json",
+    ".eslintrc",
+  },
+  settings = {
+    workingDirectories = { mode = "auto" },
+  },
+}
