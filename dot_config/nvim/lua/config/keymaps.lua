794a0b1e9027e5468f7e3c60df1adf45a79f3787
이규환 <lgh778923@gmail.com>
2026-03-31 14:51:25 +0900

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -13,6 +13,12 @@ map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "다음 버퍼" })
 -- ESC로 검색 하이라이트 제거
 map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "검색 하이라이트 제거" })
 
+-- 경로 복사
+map("n", "<leader>yp", function() vim.fn.setreg("+", vim.fn.expand("%")) end, { desc = "상대 경로 복사" })
+map("n", "<leader>yP", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = "절대 경로 복사" })
+map("n", "<leader>yf", function() vim.fn.setreg("+", vim.fn.expand("%:t")) end, { desc = "파일명 복사" })
+map("n", "<leader>yl", function() vim.fn.setreg("+", vim.fn.expand("%") .. ":" .. vim.fn.line(".")) end, { desc = "경로:라인번호 복사" })
+
 -- 비주얼 모드에서 들여쓰기 유지
 map("v", "<", "<gv")
 map("v", ">", ">gv")
