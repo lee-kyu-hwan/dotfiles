b36e89327ae56db41c6fdf0d65928cca613b51ee
이규환 <lgh778923@gmail.com>
2026-03-31 14:43:01 +0900

    Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,54 @@ return {
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "버퍼 목록" },
       { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "도움말 검색" },
       { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "최근 파일" },
+      {
+        "<leader>fp",
+        function()
+          -- 모노레포 프로젝트 디렉토리 선택 후 파일 검색
+          local root = vim.fs.root(0, { "pnpm-workspace.yaml", ".git" }) or vim.uv.cwd()
+          require("telescope.builtin").find_files({
+            prompt_title = "프로젝트 선택",
+            cwd = root,
+            find_command = { "fd", "--type", "d", "--max-depth", "4", "--exclude", "node_modules", "--exclude", ".git", "--exclude", "dist", "--exclude", ".next" },
+            attach_mappings = function(_, map)
+              map("i", "<CR>", function(prompt_bufnr)
+                local entry = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
+                require("telescope.actions").close(prompt_bufnr)
+                require("telescope.builtin").find_files({
+                  prompt_title = entry.value .. " 파일 검색",
+                  cwd = root .. "/" .. entry.value,
+                })
+              end)
+              return true
+            end,
+          })
+        end,
+        desc = "프로젝트 디렉토리 → 파일 검색",
+      },
+      {
+        "<leader>fP",
+        function()
+          -- 모노레포 프로젝트 디렉토리 선택 후 텍스트 검색
+          local root = vim.fs.root(0, { "pnpm-workspace.yaml", ".git" }) or vim.uv.cwd()
+          require("telescope.builtin").find_files({
+            prompt_title = "프로젝트 선택 (텍스트 검색)",
+            cwd = root,
+            find_command = { "fd", "--type", "d", "--max-depth", "4", "--exclude", "node_modules", "--exclude", ".git", "--exclude", "dist", "--exclude", ".next" },
+            attach_mappings = function(_, map)
+              map("i", "<CR>", function(prompt_bufnr)
+                local entry = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
+                require("telescope.actions").close(prompt_bufnr)
+                require("telescope.builtin").live_grep({
+                  prompt_title = entry.value .. " 텍스트 검색",
+                  cwd = root .. "/" .. entry.value,
+                })
+              end)
+              return true
+            end,
+          })
+        end,
+        desc = "프로젝트 디렉토리 → 텍스트 검색",
+      },
     },
     config = function()
       local telescope = require("telescope")
